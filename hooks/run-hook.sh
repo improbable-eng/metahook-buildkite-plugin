64db60ad043305c48e6a0f8712b2af575da0440e
Peter Mounce <pete@neverrunwithscissors.com>
2020-06-02 12:42:23 +0100

    restore comment

--- hooks/run-hook.sh
+++ hooks/run-hook.sh
@@ -8,6 +8,8 @@ var_name="BUILDKITE_PLUGIN_METAHOOK_${upperd}"
 if grep -q "${var_name}" <"${BUILDKITE_METAHOOK_HOOKS_PATH}/vars"; then
   hook_file="${BUILDKITE_METAHOOK_HOOKS_PATH}/${hook_name}"
 
+  # Exclamation syntax here is dynamic variable expansion.
+  # That is, use the var_name string to look up the value.
   echo "${!var_name}" >"${hook_file}"
   chmod +x "${hook_file}"
   "${hook_file}"
