bb69f704337ee4f42e31a6a244bfbc1903370933
Peter Mounce <petermounce@improbable.io>
2020-06-02 15:36:45 +0100

    bash strict mode + exec

--- hooks/run-hook.sh
+++ hooks/run-hook.sh
@@ -8,9 +8,13 @@ var_name="BUILDKITE_PLUGIN_METAHOOK_${upperd}"
 if grep -q "${var_name}" <"${BUILDKITE_METAHOOK_HOOKS_PATH}/vars"; then
   hook_file="${BUILDKITE_METAHOOK_HOOKS_PATH}/${hook_name}"
 
+  echo "#\!/usr/bin/env bash" >"${hook_file}"
+  echo "set -o errexit" >>"${hook_file}"
+  echo "set -o nounset" >>"${hook_file}"
+  echo "set -o pipefail" >>"${hook_file}"
   # Exclamation syntax here is dynamic variable expansion.
   # That is, use the var_name string to look up the value.
-  echo "${!var_name}" >"${hook_file}"
+  echo "${!var_name}" >>"${hook_file}"
   chmod +x "${hook_file}"
-  "${hook_file}"
+  exec "${hook_file}"
 fi
